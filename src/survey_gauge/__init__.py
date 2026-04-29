189a0dc50ad22dc9e0b6b9dc5ec0af7f787597af
figinii <matteo.figini.02@gmail.com>
2026-04-29 16:07:26 -0500

    feat: add survey gauge example

--- src/survey_gauge/__init__.py
+++ src/survey_gauge/__init__.py
@@ -1,6 +1,7 @@
 from .Data import Questionnaire
 from .Data import Scenario
 from .Evaluator import Eval
+from .SurveyServer import SurveyServer
 
 def main() -> None:
     print("Package survey_gauge is ready 🚀.")
